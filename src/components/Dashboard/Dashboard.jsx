a9d2516bf8ce737a2a02442a562cb8e9e83cbe19
Alexeygmr <alexey@cravter.com>
2022-05-19 17:49:12 +0300

    add bugfix

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Dashboard.module.scss'
 import Menu from "../Menu/Menu";
 import DashBoardHeader from "../DashboardHeader/DashboardHeader";
 import DashboardCharts from "../DashboardCharts/DashboardCharts";
 import {ReactComponent as LeftArrowBtn} from "../../assets/arrowLeftButton.svg";
+import useWindowDimensions from "../../hooks/useWindowDimension";
 
 const Dashboard = () => {
-  const [isShowMenu, setIsShowMenu] = useState(true);
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const { width } = useWindowDimensions();
+
+  useEffect(()=>{
+    if(width>1023){
+      setIsShowMenu(true)
+    }
+  },[])
+
   return (
     <div className={styles.dashboardMainConitainer}>
       {isShowMenu ? <Menu setIsShowMenu={setIsShowMenu}/> : <button className={styles.dashboardMainConitainerShowMenuBtn} onClick={()=>setIsShowMenu(prevState => !prevState)}><LeftArrowBtn/></button>}
